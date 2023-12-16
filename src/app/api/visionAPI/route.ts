c7e88d3b4456555f2e9009d55700e6410e516467
ka1ut <tka1utjp@gmail.com>
2023-12-17 03:13:10 +0900

    [fix]openai api

--- src/app/api/visionAPI/route.ts
+++ src/app/api/visionAPI/route.ts
@@ -1,52 +1,52 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET( req: NextApiRequest,res: NextApiResponse){
-  console.log(req)
-    const base64Image = req.body.url;
 
-    console.log("VisionjjjjApi")
-    const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-    if (!apiKey) {
-        throw new Error("API key not found");
-    }
-
-    const prompt = `
-    You generate the questions. This image contains social issues.
-
-    # Output Example
-    - 雨はどのような影響を人に与える？
-    - なぜ太陽は沈むの？
-    
-    # Careful.
-    - output is Japanese
-    - Don't give answers
-    - Ask unique questions
-    - A curious point of view`;
-
-    const payload = {
-        model: "gpt-4-vision-preview",
-        messages: [
-            {
-                role: "user",
-                content: [
-                    { 
-                      type: "text",
-                      text: prompt 
-                    },
-                    { 
-                      type: "image_url",
-                      image_url: {
-                        url: `data:image/jpeg;base64${base64Image}`
-                      } 
-                    }
-                ]
-            }
-        ],
-        max_tokens: 300
-    };
-    console.log(payload)
-
+export async function POST(req: NextRequest) {
     try {
+        const body = await req.json();
+        const base64Image = body.base64;
+        if(!base64Image){
+            return NextResponse.json({error: "base64Image not found"}, { status: 500})
+        }
+           
+        const apiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+        if (!apiKey) {
+            return NextResponse.json({error: "API key not found"}, { status: 500})
+        }
+
+        const prompt = `
+        You generate the questions. This image contains social issues.
+
+        # Output Example
+        - 雨はどのような影響を人に与える？
+        - なぜ太陽は沈むの？
+        
+        # Careful.
+        - output is Japanese
+        - Don't give answers
+        - Ask unique questions
+        - A curious point of view`
+
+        const payload = {
+            model: "gpt-4-vision-preview",
+            messages: [
+                {
+                    role: "user",
+                    content: [
+                        { 
+                            type: "text",
+                            text: prompt 
+                        },
+                        { 
+                            type: "image_url",
+                            image_url: base64Image
+                        } 
+                    ]
+                }
+            ],
+            max_tokens: 300
+        };
+
         const response = await fetch("https://api.openai.com/v1/chat/completions", {
             method: 'POST',
             headers: {
@@ -55,16 +55,25 @@ export async function GET( req: NextApiRequest,res: NextApiResponse){
             },
             body: JSON.stringify(payload)
         });
-
+        
         if (!response.ok) {
-            console.log("HTTP error! status", response.status)
-            res.status(500).json({ error: "HTTP error! status" });
+            return NextResponse.json({error: "HTTP error"}, { status: response.status})
         }
-        const data = await response.json();
-        console.log(data)
-        res.status(200).json(data);
+
+        const json_data = await response.json();
+        const response_data = json_data.choices[0].message.content;
+        return NextResponse.json(response_data, { status: 200})
+
     } catch (error) {
-        console.error("There was an error!", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    } 
-};
+        return NextResponse.json({error: "Internal Server Error"}, { status: 500})
+    }
+}
+
+export async function GET() {
+    return new NextResponse(JSON.stringify({ message: 100 }), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
