642855c61ceb3a365cd7e9623a60d522fd0271be
ka1ut <tka1utjp@gmail.com>
2023-12-17 15:48:43 +0900

    [change]dont show console.log(base64Image)

--- src/app/api/visionApi/route.ts
+++ src/app/api/visionApi/route.ts
@@ -5,7 +5,6 @@ export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
         const base64Image = body.base64;
-        console.log(base64Image)
         
         if(!base64Image){
             return NextResponse.json({error: "base64Image not found"}, { status: 500})
